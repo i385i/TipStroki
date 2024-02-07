f0e6da0c79b941477c1e4ba26e6f04f19b7c7d51
niagarskiy <niagarskiy@MY-PC>
2024-02-07 21:10:13 +0300

    Добавьте файлы проекта.

---
+++ TipStroki/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TipStroki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1 Задание
+            /*
+            string[] textMass;
+            string text = ("Желаю, чтобы этот день запомнился только приятными впечатлениями, яркими красками, добротой и отзывчивостью окружающих, интересными встречами и, конечно же, выполненными задачами.");
+            textMass = text.Split(' ');
+            Console.WriteLine("Количество слов: " + textMass.Length);
+            Console.ReadLine();
+            */
+            #endregion 1 Задание 
+
+            #region 2 Задание
+            /*
+            string[] people = { "Дом", "Джинсы", "Каша", "Помидор", "Торт", "Алфавит", "Строка", "Чашка", "Кофе", "Баклажан" };
+            var orderedPeople = from p in people orderby p select p;
+            foreach (var p in orderedPeople)
+                Console.WriteLine(p);
+            */
+            #endregion 2 Задание
+        }
+    }
+}
